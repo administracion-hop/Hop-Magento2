67c09634113787abfe32691283d62664ec86e28b
angel coyasos <angelcotr@gmail.com>
2025-10-28 09:30:26 -0500

    fix: indefined global varible

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,31 +1,9 @@
-var hop_amasty_mixin_enabled = !window.hop.amasty_checkout_disabled,
-    config;
-
 var config = {
     config: {
         mixins: {
             'Magento_Checkout/js/view/shipping': {
                 'Hop_Envios/js/view/shipping': true
-            },
-            'Amasty_CheckoutCore/js/model/one-step-layout': {
-                'Hop_Envios/js/model/one-step-layout-mixin': hop_amasty_mixin_enabled
-            },
-            'Amasty_CheckoutCore/js/model/shipping-rate-service-override': {
-                'Hop_Envios/js/model/amasty-shipping-rate-service-mixin': hop_amasty_mixin_enabled
             }
         }
-    },
-    'map': { '*': {} },
-/*     map: {
-        '*': {
-            'Amasty_CheckoutCore/onepage/shipping/methods':
-                'Hop_Envios/onepage/shipping/methods'
-        }
-    } */
+    }
 };
-
-if (hop_amasty_mixin_enabled) {
-    config.map['*'] = {
-        'Amasty_CheckoutCore/onepage/shipping/methods': 'Hop_Envios/onepage/shipping/methods'
-    };
-}
--- view/frontend/web/js/hopAmastyCheckoutEnabled.js
+++ view/frontend/web/js/hopAmastyCheckoutEnabled.js
@@ -1,3 +1,22 @@
 // Desactiva mixins/overrides dependientes de Amasty
 window.hop = window.hop || {};
 window.hop.amasty_checkout_disabled = false;
+
+require.config({
+  config: {
+    mixins: {
+      'Amasty_CheckoutCore/js/model/one-step-layout': {
+        'Hop_Envios/js/model/one-step-layout-mixin': true
+      },
+      'Amasty_CheckoutCore/js/model/shipping-rate-service-override': {
+        'Hop_Envios/js/model/amasty-shipping-rate-service-mixin': true
+      }
+    }
+  },
+  map: {
+    '*': {
+      'Amasty_CheckoutCore/js/onepage/shipping/methods':
+        'Hop_Envios/js/onepage/shipping/methods'
+    }
+  }
+});
